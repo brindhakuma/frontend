bc265ee0575c0bc19bb51b7c95f867897edd8772
brindhakuma <160593693+brindhakuma@users.noreply.github.com>
2026-02-10 17:57:18 +0530

    Set default API_BASE_URL for development

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 // const API_BASE_URL = "https://sailaya.pythonanywhere.com;
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+// const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://sailaya.pythonanywhere.com";
 
 export async function registerUser(data) {
   const response = await fetch(`${API_BASE_URL}/register`, {
