ab566c42eafd64f91e6d1c3cd04cebfeb607cd19
brindhakuma <160593693+brindhakuma@users.noreply.github.com>
2026-02-10 17:52:25 +0530

    Update API base URL to use environment variable

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://sailaya.pythonanywhere.com";
+// const API_BASE_URL = "https://sailaya.pythonanywhere.com;
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export async function registerUser(data) {
   const response = await fetch(`${API_BASE_URL}/register`, {
